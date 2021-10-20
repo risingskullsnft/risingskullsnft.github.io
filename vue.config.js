59581d372e3167507221f672c84ac9d32db20234
Rising Skulls NFT <risingskullsnft@gmail.com>
2021-10-20 20:49:03 +0200

    remove unneccessary config

--- vue.config.js
+++ vue.config.js
@@ -2,28 +2,4 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "./docs"),
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/scss/main.scss";`,
-      },
-    },
-  },
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.glsl$/,
-          use: "raw-loader",
-        },
-      ],
-    },
-    resolve: {
-      alias: {
-        js: path.resolve(__dirname, "src/js/"),
-        components: path.resolve(__dirname, "src/components/"),
-        views: path.resolve(__dirname, "src/views"),
-      },
-    },
-  },
 };
